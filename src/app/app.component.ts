aaaefca1ad434d586fa6163de1286c6e0bc53f46
tainan21 <tainankarate@gmail.com>
2023-10-19 02:34:14 -0300

    Alter realter reset soft - Menu, Header, Projects, About, footer and corect jasmine and alter remove angulaCache

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
+import { Title, Meta } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
+import {LanguageService} from "src/app/services/language/language.service"
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'portfolioAngular';
+export class AppComponent implements OnInit{
+  title = 'TainanCamargo - Portfolio';
+  
+  constructor(
+    private titleService: Title,
+    private metaService: Meta,
+    private translateService: TranslateService,
+    private location: Location,
+    private languageService: LanguageService
+    ){
+    }
+  ngOnInit(): void{
+    
+    this.languageService.initLanguage()
+
+
+    this.titleService.setTitle( "Tainan Camargo| FullStack Developer" );
+
+    this.metaService.addTags([
+      {name: 'keywords', content: 'Frontend, software, developer'},
+      {name: 'description', content: ''},
+    ]);
+    
+    
+    AOS.init(); 
+
+  }
 }
